27228d4eea269e60a4f617c689ea415d9c1102f6
Shubham 10000 <sawant.shubham1102@gmail.com>
2026-04-22 12:24:22 +0530

    made the appropriate changes

--- python-service/app.py
+++ python-service/app.py
@@ -1,17 +1,25 @@
-from parser import extract_text_from_pdf_file
+from fastapi import FastAPI
+from pydantic import BaseModel
 from analyze import analyze
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    resume_path = "/Users/bunny/Documents/Colabration_projects/Resume_analyzer/python-service/Shubham_Sawant_Resume.pdf"
 
-    resume_text = extract_text_from_pdf_file(resume_path)
+class AnalyzeRequest(BaseModel):
+    resume_text: str
+    job_description: str
+    
+class AnalyzeResponse(BaseModel):
+    score: int
+    matched_skills: list[str]
+    missing_skills: list[str]
+    
+    
+@app.get('/')
+def home():
+    return {"message": "Welcome to the Resume Analyzer API!"}
 
-    job_description = """
-    Looking for a Python developer with experience in Amazon Web Services,
-    Docker, REST APIs, and Machine Learning
-    """
-
-    result = analyze(resume_text, job_description)
-
-    print(result)
+@app.post('/analyze',response_model=AnalyzeResponse)
+def analyze_resume(request:AnalyzeRequest):
+    result = analyze(request.resume_text,request.job_description)
+    return result
